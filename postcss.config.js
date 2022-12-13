f1bcb84459b975a8d2f3b7a230b3d3686a1481ff
Xinru Lu <865666015@qq.com>
2022-12-13 22:30:39 +0800

    fixed bug

---
+++ postcss.config.js
@@ -0,0 +1,17 @@
+{
+    unitToConvert: 'px',
+    viewportWidth: 1920,
+    unitPrecision: 5,
+    propList: ['*'],
+    viewportUnit: 'vw',
+    fontViewportUnit: 'vw',
+    selectorBlackList: [],
+    minPixelValue: 1,
+    mediaQuery: false,
+    replace: true,
+    exclude: undefined,
+    include: undefined,
+    landscape: false,
+    landscapeUnit: 'vw',
+    landscapeWidth: 568
+  }
--- src/views/screen/index.vue
+++ src/views/screen/index.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="page_root">
     <section class="map"></section>
+
+    <section class="mask">
+      <img class="top" src="@/assets/images/screen/on.png" alt="" />
+      <img class="right" src="@/assets/images/screen/right.png" alt="" />
+      <img class="bottom" src="@/assets/images/screen/Under.png" alt="" />
+      <img class="left" src="@/assets/images/screen/left.png" alt="" />
+    </section>
   </div>
 </template>
 
@@ -22,8 +29,46 @@ function goTarget(url) {
     position: absolute;
     width: 100%;
     height: 100%;
+    top: 0;
+    left: 0;
     background-size: 100% 100%;
-    background: url('@/assets/images/screen/bj.png') no-repeat;
+    background: url("@/assets/images/screen/bj.png") no-repeat;
+  }
+
+  .mask {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+
+    .top {
+      position: absolute;
+      width: 100%;
+      top: 0;
+      left: 0;
+    }
+
+    .right {
+      position: absolute;
+      height: 100%;
+      top: 0;
+      right: 0;
+    }
+
+    .bottom {
+      position: absolute;
+      width: 100%;
+      bottom: 0;
+      left: 0;
+    }
+
+    .left {
+      position: absolute;
+      height: 100%;
+      top: 0;
+      left: 0;
+    }
   }
 }
 </style>
